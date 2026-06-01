f02df54d26f4505f9c2bc615c3d0d666f710b5a1
M Ilkham Fauzi <ilham@Ms-MacBook-Pro.local>
2026-06-01 10:36:32 +0700

    chore: bump rcp-light to v1.5.0

--- Casks/rcp-light.rb
+++ Casks/rcp-light.rb
@@ -1,6 +1,6 @@
 cask "rcp-light" do
-  version "1.4.2"
-  sha256 "8c478a89ab2533498b6ba75eb05ab93275e7ddf7f49b9f8c9ec16bf6205dfe72"
+  version "1.5.0"
+  sha256 "bf5b3df0873104f9a6c2ddfd0025b52dc74777105a3a01c224d3f38c40e8731c"
 
   url "https://github.com/ilham-fauzi/RCP-Light/releases/download/v#{version}/rcp-light-v#{version}.zip"
   name "RCP Light"
